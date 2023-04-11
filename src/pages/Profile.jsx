4f863f5bd9129747d2575b457d54a9585766728d
grmbyrn <graemebyrne_89@hotmail.com>
2023-04-11 12:43:13 +0200

    add firebase to project and utilise it in sign up and sign in pages

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,14 @@
+import {getAuth} from 'firebase/auth'
+import { useEffect, useState } from 'react'
+
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
+  const [user, setUser] = useState(null)
+
+  const auth = getAuth()
+  useEffect(() => {
+    console.log(auth.currentUser)
+  }, [])
+
+  return user ? <h1>{user.displayName}</h1> : 'Not Logged In'
 }
 export default Profile
